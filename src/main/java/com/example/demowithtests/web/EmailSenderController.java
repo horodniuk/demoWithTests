6e03ff2b08f69d056aaee2fcd29a2387acf78da0
horodniuk <horodniuk.maksym55@gmail.com>
2023-06-28 02:39:41 +0300

    update sendEmail with body

--- src/main/java/com/example/demowithtests/web/EmailSenderController.java
+++ src/main/java/com/example/demowithtests/web/EmailSenderController.java
@@ -1,13 +1,11 @@
 package com.example.demowithtests.web;
 
 
+import com.example.demowithtests.service.email.EmailMessage;
 import com.example.demowithtests.service.email.EmailSenderService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @AllArgsConstructor
@@ -18,9 +16,9 @@ public class EmailSenderController {
     private final EmailSenderService emailSenderService;
 
     @PostMapping("/send")
-    public void sendEmail(@RequestParam String email,
-                          @RequestParam String body,
-                          @RequestParam String subject) {
-      emailSenderService.sendEmail(email, body, subject);
+    public void sendEmail(@RequestBody EmailMessage message) {
+       emailSenderService.sendEmail(message.getEmail(),
+                                    message.getBody(),
+                                    message.getSubject());
     }
 }
